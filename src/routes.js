ed9d5ac7ef2261f3c50229295b7bbf82e0dde93c
Pavel Popeskul <peterjj91@gmail.com>
2020-02-08 17:41:46 +0200

    Setup topbar & pages

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import GlobalFeed from "pages/GlobalFeed";
+import Article from "pages/Article";
+import Authentication from "pages/Authentication";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" component={GlobalFeed} exact />
+      <Route path="/articles/:slug" component={Article} />
+      <Route path="/login" component={Authentication} />
+    </Switch>
+  );
+};
+export default Routes;
